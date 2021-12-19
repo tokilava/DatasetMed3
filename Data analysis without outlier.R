d44c8cdb5cdc0e32de7c3c7df9a31ac5ca673a20
LavaOlsen <79588155+LavaOlsen@users.noreply.github.com>
2021-12-19 15:51:50 +0100

    har removed participant 6 som er vores outlier

---
+++ Data analysis without outlier.R
@@ -0,0 +1,212 @@
+# Data Analysis uden participant 6
+
+
+# Here is a link to a lot of different statistical analyses you can do in R. Start by looking at the BCI papers what type of test do they use? It would make sense to use the same type as them. https://stats.idre.ucla.edu/r/whatstat/what-statistical-analysis-should-i-usestatistical-analyses-using-r/
+
+
+#------- libraries ------------
+library(tidyverse)
+library(magrittr)
+
+#------- Load Data ------------
+load("simple_data_Clean.rda")
+load("merged_data_Clean.rda")
+
+#------- Order of PAMs --------
+
+# Does the test order of the PAM influence the fustration?
+TestIDandFrustration <- dfStat_remove %>%
+  group_by(TestID) %>%
+  summarise(meanFrustration = mean(Frustration),
+            medianFrustration = median(Frustration),
+            sdFrustration = sd(Frustration))
+
+TestIDandFrustration
+
+summary(lm(Frustration ~ TestID, data = dfStat_remove))
+
+# Does the test order of the PAM influence the Control?
+TestIDandControl <- dfStat_remove %>%
+  group_by(TestID) %>%
+  summarise(meanControl = mean(Control),
+            medianControl = median(Control),
+            sdControl = sd(Control))
+
+TestIDandControl
+
+summary(lm(Control ~ TestID, data = dfStat_remove))
+
+# Does the test order of the PAM influence the Control
+TestIDandRank <- dfStat_remove %>%
+  dplyr::group_by(TestID) %>%
+  summarise(meanRank = mean(Rank),
+            medianRank = median(Rank),
+            sdRank = sd(Rank))
+
+TestIDandRank
+
+summary(lm(Rank ~ TestID, data = dfStat_remove))
+
+#------- Frustration --------
+FrustrationOverview <- dfStat_remove %>%
+  group_by(PAM) %>%
+  summarise(meanFrustration = mean(Frustration),
+            medianFrustration = median(Frustration),
+            sdFrustration = sd(Frustration))
+
+FrustrationOverview
+
+dfStat %>% 
+  ggplot(aes(x = PAM, y = Frustration, color = PAM)) +
+  geom_violin() + 
+  geom_jitter(shape=16, position=position_jitter(0.2)) +
+  theme_classic()
+
+# PAMs Influence on Fustration
+summary(lm(Frustration ~ PAM, data = dfStat_remove))
+
+#------- Control --------
+
+ControlOverview <- dfStat_remove %>%
+  group_by(PAM) %>%
+  summarise(meanControl = mean(Control),
+            medianControl = median(Control),
+            sdControl = sd(Control))
+
+ControlOverview
+
+# PAMs Influence on Control
+summary(lm(Control ~ PAM, data = dfStat_remove))
+
+#------- Rank --------
+
+RankOverview <- dfStat_remove %>%
+  dplyr::group_by(PAM) %>%
+  summarise(meanRank = mean(Rank),
+            medianRank = median(Rank),
+            sdRank = sd(Rank))
+
+RankOverview
+
+dfStat_remove %>% 
+  filter(PAM != "Training") %>%
+  ggplot(aes(x = PAM, y = Rank, color = PAM)) +
+  geom_violin() +
+  theme_classic()
+
+#------- Frustration vs Control --------
+
+
+#REMOVING PARTICIPANT 6 (OUTLIAR)
+
+dfStat_remove <- dfStat %>% 
+  filter(ParticipantID != 6)
+
+library(ggrepel)
+
+dfStat_remove %>% 
+  ggplot(aes(x = Control, y = Frustration, color = PAM)) +
+  geom_jitter() +
+  geom_smooth(method = lm, se=F) +
+  theme_classic() 
+
+
+
+
+# if we look at all the data
+summary(lm(Frustration ~ Control, data = dfStat_remove))
+
+# If we split them up 
+summary(lm(Frustration ~ Control * PAM, data = dfStat_remove))
+
+#------- Frustration vs Rank --------
+
+dfStat_remove %>% 
+  ggplot(aes(x = Rank, y = Frustration, #color = PAM
+  )) +
+  geom_jitter() +
+  geom_smooth(method = lm, se=F) +
+  theme_classic()
+
+# if we look at all the data
+summary(lm(Frustration ~ Rank, data = dfStat_remove))
+
+# If we split them up 
+summary(lm(Frustration ~ PAM, data = dfStat_remove))
+
+# If we split them up 
+summary(lm(Frustration ~ Rank * PAM, data = dfStat_remove))
+
+#------- Frustration vs Gender --------
+
+GenderFrustrationOverview <- dfStat_remove %>%
+  group_by(Gender) %>%
+  summarise(meanFrustration = mean(Frustration, na.rm = TRUE),
+            medianFrustration = median(Frustration, na.rm = TRUE),
+            sdFrustration = sd(Frustration, na.rm = TRUE))
+
+GenderFrustrationOverview
+
+# Are males and females equal?
+summary(lm(Frustration ~ Gender, data = dfStat_remove))
+
+#------- Frustration vs Age  --------
+
+dfStat_remove %>% 
+  ggplot(aes(x = Age, y = Frustration)) +
+  geom_jitter() +
+  geom_smooth(aes(color = Gender), method = lm, se=F) +
+  #geom_smooth(aes(color = PAM), method = lm, se=F) +
+  theme_classic()
+
+# Are males and females equal?
+summary(lm(Frustration ~ Age, data = dfStat_remove))
+
+#------- Control vs Rank --------
+
+dfStat_remove %>% 
+  ggplot(aes(x = Rank, y = Control, color = PAM)) +
+  #geom_violin(shape=16, position=position_jitter(0.1)) +
+  #geom_smooth(color = "Black", method = lm, se=F) +
+  geom_jitter()+
+  geom_smooth(aes(color = PAM), method = lm, se=F) +
+  theme_classic()
+
+# if we look at all the data
+summary(lm(Control ~ Rank, data = dfStat_remove))
+
+# If we split them up 
+summary(lm(Control ~ Rank * PAM, data = dfStat_remove))
+
+
+#------- Frustration vs Rank ------
+dfStat_remove %>% 
+  ggplot(aes(x = Rank, y = Frustration)) +
+  #geom_violin(shape=16, position=position_jitter(0.1)) +
+  #geom_smooth(color = "Black", method = lm, se=F) +
+  #geom_jitter()+
+  geom_smooth(aes(color = PAM), method = lm, se=F) +
+  theme_classic()
+
+# if we look at all the data
+summary(lm(Frustration ~ Rank, data = dfStat_remove))
+
+# If we split them up 
+summary(lm(Frustration ~ Rank * PAM, data = dfStat_remove))
+#------- Control vs Gender --------
+
+GenderControlOverview <- dfStat %>%
+  group_by(Gender) %>%
+  summarise(meanControl = mean(Control, na.rm = TRUE),
+            medianControl = median(Control, na.rm = TRUE),
+            sdControl = sd(Control, na.rm = TRUE))
+
+GenderControlOverview
+
+dfStat %>% 
+  ggplot(aes(x = Gender, y = Control)) +
+  geom_violin(shape=16, position=position_jitter(0.1)) +
+  theme_classic()
+
+# Are men and women equal?
+summary(lm(Control ~ Gender, data = dfStat))
